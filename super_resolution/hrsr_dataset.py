3b2f1a9e21fbe20fbdd37ddc9e3a35d9ca1f6c68
Shengqiu Li <lishengqiu.hit@gmail.com>
2021-06-09 08:18:52 +0000

    Update

--- super_resolution/hrsr_dataset.py
+++ super_resolution/hrsr_dataset.py
@@ -6,22 +6,28 @@ from torchvision import transforms
 
 
 class HrsrDataset(Dataset):
-    def __init__(self, file_names, lr_folder, hr_folder, target_size):
+    def __init__(self, file_names, lr_folder, hr_folder, target_size, resize_lr=True):
         self.file_names = file_names
         self.lr_folder = lr_folder
         self.hr_folder = hr_folder
         self.target_size = target_size
+        self.resize_lr = resize_lr
 
     def __getitem__(self, i):
         lr_image = Image.open(os.path.join(self.lr_folder, self.file_names[i]))
         
-        if self.hr_folder != None:
-            hr_image = Image.open(os.path.join(self.hr_folder, self.file_names[i]))
+        hr_image = None if self.hr_folder == None else Image.open(
+            os.path.join(self.hr_folder, self.file_names[i]))
 
-        lr_image = lr_image.resize(self.target_size)
+        # print(self.file_names[i])
+        if self.resize_lr:
+            lr_image = lr_image.resize(self.target_size)
         lr_tensor = transforms.ToTensor()(lr_image)
-        hr_tensor = None if hr_image == None else transforms.ToTensor()(hr_image)
-        return (lr_tensor, hr_tensor, self.file_names[i])
+        if hr_image != None:
+            hr_tensor = transforms.ToTensor()(hr_image)
+            return (lr_tensor, hr_tensor, self.file_names[i])
+        else:
+            return (lr_tensor, self.file_names[i])
 
     def __len__(self):
         return len(self.file_names)
--- super_resolution/infer.py
+++ super_resolution/infer.py
@@ -1,20 +1,22 @@
 from __future__ import absolute_import
 import os
 import random
+from numpy import save
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import transforms
+from torchvision.utils import save_image
 from hrsr_dataset import HrsrDataset
 from algo.srcnn import SRCNN
 
 INFER_LR_FOLDER = "../data/Infer_Input/"
 INFER_OUTPUT_FOLDER = "../data/Infer_Output/"
-BATCH_SIZE = 3
+BATCH_SIZE = 1
 model = SRCNN()
 MODEL_LOAD_FOLDER = "../models/"
-MODEL_NAME = "SRCNN_B3_E10.ptm"
+MODEL_NAME = "SRCNN_B2_E3.ptm"
 
 ##################################################################################
 
@@ -29,20 +31,26 @@ model = model.to(device)
 
 
 def main():
-    files = os.listdir(INFER_LR_FOLDER)
-    infer_dataset = HrsrDataset(files, INFER_LR_FOLDER, None, (3840, 2160))
+    files = [f for f in os.listdir(INFER_LR_FOLDER) if not f.startswith('.')]
+    infer_dataset = HrsrDataset(files, INFER_LR_FOLDER, None, (1920, 1080))
     infer_loader = DataLoader(infer_dataset, BATCH_SIZE, shuffle=False)
 
     model.load_state_dict(torch.load(model_file))
     model.eval()
 
     with torch.no_grad():
-        for X, y, file_name in infer_loader:
+        for X, file_name in infer_loader:
             X = X.to(device)
             pred = model(X)
-            im = transforms.ToPILImage()(pred)
-            im.save(os.path.join(INFER_OUTPUT_FOLDER, file_name), "PNG")
+            # im = transforms.ToPILImage()(pred[0].cpu().data)
 
+            print(f"Saving {file_name[0]}")
+            # im.save(os.path.join(INFER_OUTPUT_FOLDER, file_name[0]), "PNG")
+            save_decoded_image(pred.cpu().data, os.path.join(INFER_OUTPUT_FOLDER, file_name[0]))
+
+def save_decoded_image(img, name):
+    img = img.view(img.size(0), 3, 1080, 1920)
+    save_image(img, name)
 
 if __name__ == "__main__":
     main()
